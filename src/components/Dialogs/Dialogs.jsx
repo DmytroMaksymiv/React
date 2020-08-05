997e0a04692bbf03350c474655bc41e1552a5f22
dmytro_maksymiv <maksimivdmitro88@ukr.net>
2020-08-05 07:45:50 -0400

    Lesssons 24 - Separate Data to New Objekt + Doit New Arrey(Masiv)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -14,7 +14,6 @@ const DialogItem = (props) => {
     );
 }
 
-
 const Message = (props) => {
 
     return (
@@ -23,32 +22,55 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Dmytro'},
+        {id: 2, name: 'Irinka'},
+        {id: 3, name: 'Snizanna'},
+        {id: 4, name: 'Kata'},
+        {id: 5, name: 'Vika'},
+        {id: 6, name: 'Valera'},
+        {id: 7, name: 'Ivanka'},
+        {id: 8, name: 'Taras'},
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are your'},
+        {id: 3, message: 'Very Nice'},
+        {id: 4, message: 'Nice Job'},
+        {id: 5, message: 'Very Grete'},
+        {id: 6, message: 'I like BigBurger'},
+        {id: 7, message: 'Nice to met you'},
+        {id: 8, message: 'I\'m done'},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <h1>
-                    <DialogItem name="Dmytro" id="1"/>
-                    <DialogItem name="Petro" id="2"/>
-                    <DialogItem name="Sveta" id="3"/>
-                    <DialogItem name="Julia" id="4"/>
-                    <DialogItem name="Vika" id="5"/>
-                    <DialogItem name="Valera" id="6"/>
-                    <DialogItem name="Ivanka" id="7"/>
-                    <DialogItem name="Taras" id="8"/>
+                    <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                    <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                    <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                    <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                    <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                    <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
+                    <DialogItem name={dialogsData[6].name} id={dialogsData[6].id}/>
+                    <DialogItem name={dialogsData[7].name} id={dialogsData[7].id}/>
                 </h1>
             </div>
 
 
             <div className={s.messages}>
                 <h1>
-                    <Message message="Hello"/>
-                    <Message message="How are your"/>
-                    <Message message="Very Nice"/>
-                    <Message message="Nice Job"/>
-                    <Message message="Very Grete"/>
-                    <Message message="I like BigBurger"/>
-                    <Message message="Nice to met you"/>
-                    <Message message="I'm done"/>
+                    <Message message={messagesData[0].message}/>
+                    <Message message={messagesData[1].message}/>
+                    <Message message={messagesData[2].message}/>
+                    <Message message={messagesData[3].message}/>
+                    <Message message={messagesData[4].message}/>
+                    <Message message={messagesData[5].message}/>
+                    <Message message={messagesData[6].message}/>
+                    <Message message={messagesData[7].message}/>
                 </h1>
             </div>
 
--- src/components/Navbar/Profile/MyPosts/MyPosts.jsx
+++ src/components/Navbar/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,22 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-    debugger;
+
+    let postData = [
+        {id: 1, message: 'Hi, how are you?', like: 445458, location: 'Lviv', company:'Wokreyn'},
+        {id: 2, message: 'It\'s my first post', like: 5678944467, location: 'Kyiv', company:'Start Up Forum'},
+        {id: 3, message: 'It\'s very nice funktion props', like: 567, location: 'Uzgorod', company:'Mariana company'},
+        {id: 4, message: 'By in Fokstrot', like: 431, location: 'Rivne', company:'Appler'},
+        {id: 5, message: 'We ned JSX Developer', like: 1141, location: 'Spas', company:'Kuzdei Inc'},
+        {id: 6, message: 'We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood/ Whats wrong with you to day?Heeey ..We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood',
+            like: 81, location: 'Lviv', company:'Wokreyn'},
+        {id: 7, message: 'We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood/ Whats wrong with you to day?Heeey ..We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood',
+            like: 901, location: 'Hrabiv', company:'VOVA BABIN Inc'},
+        {id: 8, message: 'It\'s done', like: 401, location: 'Roznativ', company:'Spaser Z LLC'},
+
+    ]
+
+
     return (
         <div className={s.MyPosts}>
             <div>
@@ -16,18 +31,14 @@ const MyPosts = (props) => {
                 <button>Add Post</button>
             </div>
             <div className={s.posts}>
-                <Post message='Hi, how are you?' like="41" location='Lviv' company='Wokreyn'/>
-                <Post message="It's my first post" like="85" location='Kyiv' company='Start Up Forum'/>
-                <Post message="It's very nice funktion props" like="585" location='Uzgorod' company='Mariana company'/>
-                <Post message="By in Fokstrot" like="895" location='Rivne' company='Appler'/>
-                <Post message="We ned JSX Developer" like="405" location='Spas' company='Kuzdei Inc'/>
-                <Post
-                    message="We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood/ Whats wrong with you to day?Heeey ..We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood"
-                    like="85" location='Hrabiv' company='VOVA BABIN Inc'/>
-                <Post
-                    message="We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood/ Whats wrong with you to day?Heeey ..We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood We ned JSX Developer Lorem23. BJSKere movi2 / Hellow Hany moon. Nice mood"
-                    like="81125" location='Roznativ' company='Spaser Z LLC'/>
-
+                <Post message={postData[0].message} like={postData[0].like} location={postData[0].location} company={postData[0].company}/>
+                <Post message={postData[1].message} like={postData[1].like} location={postData[1].location} company={postData[1].company}/>
+                <Post message={postData[2].message} like={postData[2].like} location={postData[2].location} company={postData[2].company}/>
+                <Post message={postData[3].message} like={postData[3].like} location={postData[3].location} company={postData[3].company}/>
+                <Post message={postData[4].message} like={postData[4].like} location={postData[4].location} company={postData[4].company}/>
+                <Post message={postData[5].message} like={postData[5].like} location={postData[5].location} company={postData[5].company}/>
+                <Post message={postData[6].message} like={postData[6].like} location={postData[6].location} company={postData[6].company}/>
+                <Post message={postData[7].message} like={postData[7].like} location={postData[7].location} company={postData[7].company}/>
             </div>
         </div>
 
