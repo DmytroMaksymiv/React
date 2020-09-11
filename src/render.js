8bf556716ce6c348aeec483ae03c63a8ad6570a6
dmytro_maksymiv <maksimivdmitro88@ukr.net>
2020-09-11 16:41:16 -0400

    Lessons 33 ----ADD RERENDER + import render.js to state.js and index.js-----++++START anderstand  FLUX architecture

---
+++ src/render.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import state from "./Redux/state";
+import {addPost} from "./Redux/state";
+import {BrowserRouter} from "react-router-dom";
+
+
+
+let rerenderEntireTree = () => {
+    ReactDOM.render(
+    <BrowserRouter>
+    <React.StrictMode>
+    <App state={state} addPost={addPost} />
+    </React.StrictMode>
+    </BrowserRouter>, document.getElementById('root')
+);
+
+}
+
+
+rerenderEntireTree ();
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
