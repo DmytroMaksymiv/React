6fbad82c8389c706290308705704cac22345e34b
dmytro_maksymiv <maksimivdmitro88@ukr.net>
2020-09-21 15:40:44 -0400

    Lessons 39 - ActionCreator() + action type

--- src/components/Navbar/Profile/MyPosts/MyPosts.jsx
+++ src/components/Navbar/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../../Redux/state"
+
+
 
 const MyPosts = (props) => {
 
@@ -12,14 +15,15 @@ const MyPosts = (props) => {
 
     let addPost = () => {
         //props.addPost ();
-        props.dispatch ({ type: 'ADD-POST' });
+        props.dispatch (addPostActionCreator());
     }
 
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
         //props.updateNewPostText(text);
-        let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+        //let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch (action);
     }
 
