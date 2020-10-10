cadd03961b64f4e7521b38afe953f9580cbd43c3
dmytro_maksymiv <maksimivdmitro88@ukr.net>
2020-10-10 05:08:48 -0400

    Lessons 51 IMPORTANT(SERVER CONNECT) ------npm install axios --save--------+ JSON----+ reqest-respons + ADD PHOTO USERS

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from 'axios';
+import userPhoto from  '../../asets/images/user1.png';
 
 let Users = (props) => {
     if (props.users.length === 0) {
-
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-
-            props.setUsers()
+            props.setUsers(response.data.items)
         });
 
 
@@ -18,7 +18,7 @@ let Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={styles.userPhoto}/>
+                            <img src={ u.photos.small != null ? u.photos.small : userPhoto } className={styles.userPhoto}/>
                         </div>
                         <div>
                             {u.followed
@@ -28,12 +28,12 @@ let Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
                 </div>)
