8ebf135b0bafaea3bb36c3c86d5fa06ed6892c8d
dmytro_maksymiv <maksimivdmitro88@ukr.net>
2020-10-30 21:52:02 +0200

    Lessons 63(description)    --------DAL (Data Access Layer)

--- src/api/api.js
+++ src/api/api.js
@@ -12,7 +12,6 @@ const instance = axios.create({
 });
 
 
-
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
@@ -21,8 +20,6 @@ export const usersAPI = {
             });
     }
 }
-
-
 
 
 
