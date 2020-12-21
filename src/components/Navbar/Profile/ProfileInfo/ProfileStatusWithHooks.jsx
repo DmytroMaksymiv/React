e0dbdedfa39f8ca6605c4c587ed4e778b5c35003
dmytro_maksymiv <maksimivdmitro88@ukr.net>
2020-12-21 20:11:09 +0200

    Lessons 85 -----IMPORTANT-------hook -> useEffect

--- src/components/Navbar/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Navbar/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import s from "./ProfileInfo.module.css";
 
 let arr = [0, () => {} ];
@@ -8,6 +8,11 @@ const ProfileStatusWithHooks = (props) => {
 
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+
+    useEffect( () => {
+        setStatus(props.status);
+    }, [props.status] );
 
     const activateEditMode = () => {
         setEditMode(true);
