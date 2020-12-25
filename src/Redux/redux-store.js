df0dbb8809af8f96a1208df84a710f1724afc86f
dmytro_maksymiv <maksimivdmitro88@ukr.net>
2020-12-26 01:02:32 +0200

    Lessons 91 ------ IMPORTANT---- Chrome Extensions --- + --- React and  Redux Tools

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reduser";
 import dialogsReducer from "./dialogs-reduser";
 import sidebarReducer from "./sidebar-reduser";
@@ -20,9 +20,10 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-window.store = store;
+window.__store__ = store;
 
 
 
